013934bfeb198c31f04d80f8cf019722a6b21ed9
Venu Prasad <venuhs4@gmail.com>
2018-06-01 19:48:35 +0700

    fi

--- bot.js
+++ bot.js
@@ -1,19 +1,9 @@
 var token = '573464567:AAHbwjjA5msU3axjlrG_kgFhk8-jiprF6_o';
-var request = require('sync-request');
+var request = require('request');;
 
 var Bot = require('node-telegram-bot-api');
 var bot;
 bot = new Bot(token, { polling: true });
-
-var getResponse = function (url) {
-    var res = request('GET', url, {
-        headers: {
-            'user-agent': 'example-user-agent',
-        },
-        timeout: 5000
-    });
-    return JSON.parse(res.getBody('utf-8'));
-}
 
 console.log('bot server started...');
 
@@ -25,7 +15,10 @@ bot.onText(/^\/hi (.+)$/, function (msg, match) {
 });
 
 bot.on('text', function (msg) {
-    bot.sendMessage(msg.chat.id, getResponse("https://bitbns.com/order/getTickerWithVolume/").substring(0,100));
+    request('http://www.google.com', function (error, response, body) {
+        bot.sendMessage(msg.chat.id, JSON.stringify(body).substring(0, 100));
+    });
+
 });
 
 module.exports = bot;
