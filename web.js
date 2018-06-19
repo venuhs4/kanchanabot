ae5e2f0934eb0dd763e56ade8b37547e60e502d4
Venu Prasad <venuhs4@gmail.com>
2018-06-19 14:24:01 +0700

    test

--- web.js
+++ web.js
@@ -10,8 +10,8 @@ app.get('/', function (req, res) {
 });
 
 var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+  var host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
   console.log('Web server started at http://%s:%s', host, port);
 });
